1707db07af1448bee32951ff464a3d7e5487d05c
YeeshuPushparag <pushparagyeeshu@gmail.com>
2023-01-30 05:12:59 -0800

    hide secret key

--- Django Autorization/auth/settings.py
+++ Django Autorization/auth/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vv7*1z_#_0n+jdrl=fs2+jmem85g1ls6y1b$t(o^v_#5_e^cqq'
+SECRET_KEY = 'django-secret-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
